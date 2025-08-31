82859a200ed369ded68a3ae9c304db1e816c56e6
Naman Khater <56634706+MadMaxINDIAN@users.noreply.github.com>
2025-09-01 01:10:29 +0530

    Update application properties, enhance greeting message, and refine project structure

--- src/main/java/com/example/myFirstProject/MyClass.java
+++ src/main/java/com/example/myFirstProject/MyClass.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyClass {
     @GetMapping("path")
     public String sayHello() {
-        return "Hello World From Naman";
+        return "Hello World From Naman Khater";
     }
     
 }
