e2f95b71a7ac3a53c0dbc51a86c93dfd49bb5635
Naman Khater <56634706+MadMaxINDIAN@users.noreply.github.com>
2025-09-01 01:05:00 +0530

    Initialize Spring Boot project with essential configuration files and a sample REST controller

---
+++ src/main/java/com/example/myFirstProject/MyClass.java
@@ -0,0 +1,14 @@
+package com.example.myFirstProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MyClass {
+    @GetMapping("path")
+    public String sayHello() {
+        return "Hello World From Naman";
+    }
+    
+}
